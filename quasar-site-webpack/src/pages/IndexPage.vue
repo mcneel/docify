f9fea66f62f36c75d664f658ecbdf77ddf27734b
Morteza Karimi <morteza@mcneel.com>
2022-10-17 16:39:40 -0700

    Move source to new site

--- quasar-site-webpack/src/pages/IndexPage.vue
+++ quasar-site-webpack/src/pages/IndexPage.vue
@@ -1,17 +1,42 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page>
+    <h1>Namespaces</h1>
+      <q-item clickable v-for="item in vm" :key="item.label" @click="setSelectedItem(item.path)">
+        <q-item-section>
+          <q-item-label class="text-accent"><b>{{item.label}}</b></q-item-label>
+          <q-item-label caption>{{item.summary}}</q-item-label>
+        </q-item-section>
+      </q-item>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import ViewModel from '../ViewModel'
+import ProjInfo from '../proj_info.json'
 
-export default defineComponent({
-  name: 'IndexPage'
-})
+export default {
+  props: {
+    baseUrl: { type: String }
+  },
+  data () {
+    const tree = ViewModel.getTree()
+    return {
+      vm: tree
+    }
+  },
+  meta () {
+    return {
+      title: ProjInfo.name + ' API',
+      meta: {
+        description: { name: 'description', content: ProjInfo.description }
+      }
+    }
+  },
+  methods: {
+    setSelectedItem (name) {
+      this.$router.push(this.baseUrl + name.toLowerCase())
+      ViewModel.setSelectedItem(name)
+    }
+  }
+}
 </script>
--- quasar-site-webpack/src/router/routes.js
+++ quasar-site-webpack/src/router/routes.js
@@ -1,10 +1,31 @@
 
+const apiBase = '/api/'
+import ProjInfo from '../proj_info.json'
+
+const apiUrl = ProjInfo.name.toLowerCase()
+const apiPath = apiBase + apiUrl + '/'
+
+console.log("API PATH:", apiPath)
+
+const apiProps = {
+  apiTitle: ProjInfo.name,
+  baseUrl: apiPath
+}
+
 const routes = [
   {
-    path: '/',
+    path: apiPath,
     component: () => import('layouts/MainLayout.vue'),
+    props: apiProps,
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
+      { path: '', component: () => import('pages/IndexPage.vue'), props: apiProps },
+      { path: 'search', component: () => import('pages/SearchPage.vue') },
+      { path: 'examples/:example', component: () => import('pages/ExamplePage.vue') },
+      { path: 'whatsnew/:version', component: () => import('pages/WhatsNew.vue'), props: apiProps },
+      { path: 'references/:datatype', component: () => import('pages/ReferencesPage.vue'), props: apiProps },
+      { path: 'html/:ref', component: () => import('pages/SandcastleRedirect.vue') },
+      { path: ':datatype/:member', component: () => import('pages/MemberDetail.vue'), props: apiProps },
+      { path: ':datatype', component: () => import('pages/DataType.vue'), props: apiProps }
     ]
   },
 
