132dc20cde70202d3c4c01fe57aad7ca1b997531
Morteza Karimi <morteza@mcneel.com>
2022-10-18 16:18:05 -0700

    Add code examples

--- quasar_site/src/pages/ExamplePage.vue
+++ quasar_site/src/pages/ExamplePage.vue
@@ -13,10 +13,11 @@
     <q-separator/>
     <q-tab-panels v-model="language" animated>
       <q-tab-panel v-for="markdown in exampleMarkdown" :key="markdown.language" :name="markdown.language">
-        <q-markdown
-          no-line-numbers
-          :src="markdown.code"
-        />
+        <code-highlight :language="markdown.language">
+          <pre>
+            {{markdown.code}}
+          </pre>
+        </code-highlight>
       </q-tab-panel>
      </q-tab-panels>
   </q-page>
@@ -24,14 +25,23 @@
 
 <script>
 import Examples from '../api_examples.json'
+import CodeHighlight from "vue-code-highlight/src/CodeHighlight.vue";
+import 'prism-es6/components/prism-markup-templating';
+import 'prism-es6/components/prism-python';
+import 'prism-es6/components/prism-csharp';
+import 'prism-es6/components/prism-visual-basic';
+import "vue-code-highlight/themes/prism-tomorrow.css";
 
 export default {
   data () {
     return {
       title: '',
-      language: 'cs',
+      language: 'csharp',
       exampleMarkdown: []
     }
+  },
+  components: {
+    CodeHighlight
   },
   mounted () {
     if (this.$route.params && this.$route.params.example) {
@@ -42,11 +52,12 @@ export default {
       Examples.forEach(item => {
         if (item.name.toLowerCase().startsWith(search)) {
           // for now the javascript highlighter seems good enough
-          const code = ['```js', item.code, '```'].join('\n')
+          const code = item.code
           const index = item.name.lastIndexOf('.')
           const language = item.name.substring(index + 1)
+          const langMap = {"vb":"visual-basic", "cs": "csharp", "py":"python"}
           const md = {
-            language: language,
+            language: langMap[language],
             label: language,
             code: code
           }
